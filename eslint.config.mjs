4f098b05df6a4e9c561b0e766bc7baf16fb90164
Mithu225 <thu@opendigital.no>
2024-10-13 17:38:13 +0200

    setup eslint and prettier

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -1,9 +1,11 @@
 import globals from "globals";
 import eslintConfigPrettier from "eslint-config-prettier";
+import js from "@eslint/js";
 
 export default [
   {
     languageOptions: { globals: globals.browser },
   },
+  js.configs.recommended,
   eslintConfigPrettier,
 ];
