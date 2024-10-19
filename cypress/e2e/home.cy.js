23e463fbed5e5d0b8a57e3e21823313f86429336
Mithu225 <thu@opendigital.no>
2024-10-19 20:50:32 +0200

    workflow: Adding cypress for e2e

--- cypress/e2e/home.cy.js
+++ cypress/e2e/home.cy.js
@@ -1,18 +1,28 @@
+import { apiPath } from "../../src/js/api";
+
 describe("The user access", () => {
   it("should log in with the login form with valid credentials", () => {
     cy.fixture("validUser").then((user) => {
+      cy.intercept("POST", `${apiPath}/social/auth/login`).as("loginAttempt");
       cy.login(user.email, user.password);
       cy.get('button[data-auth="logout"]').should("exist").and("be.visible");
+
+      cy.wait("@loginAttempt").its("response.statusCode").should("eq", 200);
+      cy.window().then((window) => {
+        expect(window.localStorage.getItem("token")).to.be.not.null;
+      });
     });
   });
   it("should not log in with the login form with invalid credentials", () => {
     cy.fixture("invalidUser").then((user) => {
+      cy.intercept("POST", `${apiPath}/social/auth/login`).as("loginAttempt");
       cy.login(user.email, user.password);
       cy.on("window:alert", (t) => {
         expect(t).to.contains(
           "Either your username was not found or your password is incorrect",
         );
       });
+      cy.wait("@loginAttempt").its("response.statusCode").should("eq", 401);
     });
   });
 
@@ -26,6 +36,9 @@ describe("The user access", () => {
       cy.get("#registerForm > div.modal-header > h5").contains(
         "Create Profile",
       );
+      cy.window().then((window) => {
+        expect(window.localStorage.getItem("token")).to.be.null;
+      });
     });
   });
 });
